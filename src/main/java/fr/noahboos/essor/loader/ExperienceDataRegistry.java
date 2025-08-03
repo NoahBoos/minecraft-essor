ceebcef3970a0029434a6a843560a4317af4759a
Noah Boos <nboosgamemaker@gmail.com>
2025-08-04 00:19:39 +0200

    Fonc. - Ajout du registre JSON des barèmes d'expériences et des fichiers JSON servant de barèmes d'expériences.

---
+++ src/main/java/fr/noahboos/essor/loader/ExperienceDataRegistry.java
@@ -0,0 +1,40 @@
+package fr.noahboos.essor.loader;
+
+import fr.noahboos.essor.component.ExperienceRange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExperienceDataRegistry {
+    public static String path = "data/essor/experience/";
+
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_ARMOR;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_AXE;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_BOW;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_CROSSBOW;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_FISHING_ROD;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_FLINT_AND_STEEL;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_HOE;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_MACE;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_PICKAXE;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_SHIELD;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_SHOVEL;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_SWORD;
+    public static final Map<String, ExperienceRange> EXPERIENCE_DATA_TRIDENT;
+
+    static {
+        EXPERIENCE_DATA_ARMOR = JsonLoader.LoadExperienceData(path + "armor.json");
+        EXPERIENCE_DATA_AXE = JsonLoader.LoadExperienceData(path + "axe.json");
+        EXPERIENCE_DATA_BOW = JsonLoader.LoadExperienceData(path + "bow.json");
+        EXPERIENCE_DATA_CROSSBOW = JsonLoader.LoadExperienceData(path + "crossbow.json");
+        EXPERIENCE_DATA_FISHING_ROD = JsonLoader.LoadExperienceData(path + "fishing_rod.json");
+        EXPERIENCE_DATA_FLINT_AND_STEEL = JsonLoader.LoadExperienceData(path + "flint_and_steel.json");
+        EXPERIENCE_DATA_HOE = JsonLoader.LoadExperienceData(path + "hoe.json");
+        EXPERIENCE_DATA_MACE = JsonLoader.LoadExperienceData(path + "mace.json");
+        EXPERIENCE_DATA_PICKAXE = JsonLoader.LoadExperienceData(path + "pickaxe.json");
+        EXPERIENCE_DATA_SHIELD = JsonLoader.LoadExperienceData(path + "shield.json");
+        EXPERIENCE_DATA_SHOVEL = JsonLoader.LoadExperienceData(path + "shovel.json");
+        EXPERIENCE_DATA_SWORD = JsonLoader.LoadExperienceData(path + "sword.json");
+        EXPERIENCE_DATA_TRIDENT = JsonLoader.LoadExperienceData(path + "trident.json");
+    }
+}
