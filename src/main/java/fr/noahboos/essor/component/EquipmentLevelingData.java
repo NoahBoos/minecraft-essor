fa3de933a94169f9742b04c2926aed8b73f9b8b2
Noah Boos <nboosgamemaker@gmail.com>
2025-08-03 19:50:53 +0200

    Fonc. - Ajout de la classe EquipmentLevelingData.

---
+++ src/main/java/fr/noahboos/essor/component/EquipmentLevelingData.java
@@ -0,0 +1,40 @@
+package fr.noahboos.essor.component;
+
+public class EquipmentLevelingData {
+    private int level;
+    private int levelExperienceThreshold;
+    private float currentExperience;
+
+    // Constructeur de la classe
+    public EquipmentLevelingData(int level, int levelExperienceThreshold, float currentExperience) {
+        this.level = level;
+        this.levelExperienceThreshold = levelExperienceThreshold;
+        this.currentExperience = currentExperience;
+    }
+
+    // Getter et setter de level.
+    public int GetLevel() {
+        return level;
+    }
+    public void SetLevel(int level) {
+        this.level = level;
+    }
+
+    // Getter et setter de levelExperienceThreshold.
+    public int GetLevelExperienceThreshold() {
+        return levelExperienceThreshold;
+    }
+
+    public void SetLevelExperienceThreshold(int levelExperienceThreshold) {
+        this.levelExperienceThreshold = levelExperienceThreshold;
+    }
+
+    // Getter et setter de currentExperience.
+    public float GetCurrentExperience() {
+        return currentExperience;
+    }
+
+    public void SetCurrentExperience(float currentExperience) {
+        this.currentExperience = currentExperience;
+    }
+}
