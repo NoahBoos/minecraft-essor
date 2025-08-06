27b1f754796a044e7e5fce4ad5c77b8d053faac7
Noah Boos <nboosgamemaker@gmail.com>
2025-08-07 01:10:43 +0200

    Fonc. - Ajout de LoadRewardData() dans le chargeur JSON.

--- src/main/java/fr/noahboos/essor/loader/JsonLoader.java
+++ src/main/java/fr/noahboos/essor/loader/JsonLoader.java
@@ -2,6 +2,7 @@ package fr.noahboos.essor.loader;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import net.minecraftforge.unsafe.UnsafeFieldAccess;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -19,7 +20,22 @@ public class JsonLoader {
             Gson gson = new Gson();
             Type type = new TypeToken<Map<String, Float>>(){}.getType();
             return gson.fromJson(reader, type);
-        }  catch (Exception exception) {
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Map<String, Map<String, Integer>> LoadRewardData(String path) {
+        try (InputStream stream = JsonLoader.class.getClassLoader().getResourceAsStream(path)) {
+            if (stream == null) {
+                throw new  RuntimeException("Failed to load reward data from " + path);
+            }
+            Reader reader = new InputStreamReader(stream);
+            Gson gson = new Gson();
+            Type type = new TypeToken<Map<String, Map<String, Integer>>>(){}.getType();
+            return gson.fromJson(reader, type);
+        } catch (Exception exception) {
             exception.printStackTrace();
             return null;
         }
