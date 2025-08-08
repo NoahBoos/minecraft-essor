886f51b3354511c73cf281e6239fcaad0068a425
Noah Boos <nboosgamemaker@gmail.com>
2025-08-08 21:57:45 +0200

    Refacto. - Création de Map à registres d'enchantements typés pour la recherche par groupe d'items.

--- src/main/java/fr/noahboos/essor/registry/EnchantmentRewardDataRegistry.java
+++ src/main/java/fr/noahboos/essor/registry/EnchantmentRewardDataRegistry.java
@@ -1,30 +1,13 @@
 package fr.noahboos.essor.registry;
 
 import fr.noahboos.essor.loader.JsonLoader;
+import net.minecraft.world.item.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class EnchantmentRewardDataRegistry {
     public static String path = "data/essor/reward/enchantments";
 
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_HELMET;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_CHESTPLATE;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_LEGGINGS;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_BOOTS;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_AXE;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_BOW;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_CROSSBOW;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_FISHING_ROD;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_FLINT_AND_STEEL;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_HOE;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_MACE;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_PICKAXE;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_SHEAR;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_SHIELD;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_SHOVEL;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_SWORD;
-    public static final Map<String, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_TRIDENT;
     public static final Map<Integer, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_HELMET;
     public static final Map<Integer, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_CHESTPLATE;
     public static final Map<Integer, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_LEGGINGS;
@@ -42,6 +25,9 @@ public class EnchantmentRewardDataRegistry {
     public static final Map<Integer, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_SHOVEL;
     public static final Map<Integer, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_SWORD;
     public static final Map<Integer, Map<String, Integer>> ENCHANTMENT_REWARD_DATA_TRIDENT;
+
+    public static Map<ArmorItem.Type, Map<Integer, Map<String, Integer>>> ENCHANTMENT_REWARD_DATA_ARMOR;
+    public static Map<Class<?>, Map<Integer, Map<String, Integer>>> ENCHANTMENT_REWARD_DATA_ITEMS;
 
     static {
         ENCHANTMENT_REWARD_DATA_HELMET = JsonLoader.LoadRewardData(path + "helmet.json");
@@ -61,5 +47,22 @@ public class EnchantmentRewardDataRegistry {
         ENCHANTMENT_REWARD_DATA_SHOVEL = JsonLoader.LoadRewardData(path + "shovel.json");
         ENCHANTMENT_REWARD_DATA_SWORD = JsonLoader.LoadRewardData(path + "sword.json");
         ENCHANTMENT_REWARD_DATA_TRIDENT = JsonLoader.LoadRewardData(path + "trident.json");
+
+        ENCHANTMENT_REWARD_DATA_ARMOR.put(ArmorItem.Type.HELMET, ENCHANTMENT_REWARD_DATA_HELMET);
+        ENCHANTMENT_REWARD_DATA_ARMOR.put(ArmorItem.Type.CHESTPLATE, ENCHANTMENT_REWARD_DATA_CHESTPLATE);
+        ENCHANTMENT_REWARD_DATA_ARMOR.put(ArmorItem.Type.LEGGINGS, ENCHANTMENT_REWARD_DATA_LEGGINGS);
+        ENCHANTMENT_REWARD_DATA_ARMOR.put(ArmorItem.Type.BOOTS, ENCHANTMENT_REWARD_DATA_BOOTS);
+
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(AxeItem.class, ENCHANTMENT_REWARD_DATA_AXE);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(BowItem.class, ENCHANTMENT_REWARD_DATA_BOW);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(CrossbowItem.class, ENCHANTMENT_REWARD_DATA_CROSSBOW);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(HoeItem.class, ENCHANTMENT_REWARD_DATA_HOE);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(MaceItem.class, ENCHANTMENT_REWARD_DATA_MACE);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(PickaxeItem.class, ENCHANTMENT_REWARD_DATA_PICKAXE);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(ShearsItem.class, ENCHANTMENT_REWARD_DATA_SHEAR);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(ShieldItem.class, ENCHANTMENT_REWARD_DATA_SHIELD);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(ShovelItem.class, ENCHANTMENT_REWARD_DATA_SHOVEL);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(SwordItem.class, ENCHANTMENT_REWARD_DATA_SWORD);
+        ENCHANTMENT_REWARD_DATA_ITEMS.put(TridentItem.class, ENCHANTMENT_REWARD_DATA_TRIDENT);
     }
 }
