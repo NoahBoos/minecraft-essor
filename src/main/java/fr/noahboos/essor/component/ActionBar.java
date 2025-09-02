3ff019cffacda6e0e43be404b99246f4ea93cc11
Noah Boos <nboosgamemaker@gmail.com>
2025-09-02 19:27:18 +0200

    Correctif - Ajout de la mention "PE" dans l'Action Bar.

--- src/main/java/fr/noahboos/essor/component/ActionBar.java
+++ src/main/java/fr/noahboos/essor/component/ActionBar.java
@@ -9,7 +9,7 @@ public class ActionBar {
         EquipmentLevelingData equipmentLevelingData = itemInHand.get(ModDataComponentTypes.DC_EQUIPMENT_LEVELING_DATA);
         if (equipmentLevelingData == null) return;
 
-        String experienceProgressCount = Component.translatable("translation.level").getString() + " " + equipmentLevelingData.GetLevel() + " - " + equipmentLevelingData.GetCurrentExperience() + " / " + equipmentLevelingData.GetRequiredExperienceToLevelUp();
+        String experienceProgressCount = Component.translatable("translation.level").getString() + " " + equipmentLevelingData.GetLevel() + " - " + equipmentLevelingData.GetCurrentExperience() + " / " + equipmentLevelingData.GetRequiredExperienceToLevelUp() + " " + Component.translatable("translation.experiencePoint").getString();
         player.displayClientMessage(Component.literal(experienceProgressCount), true);
     }
 }
