81f439178266b3c39af9cca45ef0114dbd69d8da
Noah Boos <nboosgamemaker@gmail.com>
2025-09-08 10:20:44 +0200

    Refacto. - Ajout d'une classe TooltipEvents, provoquant l'allégement d'ItemEvents.

--- src/main/java/fr/noahboos/essor/event/ItemEvents.java
+++ src/main/java/fr/noahboos/essor/event/ItemEvents.java
@@ -3,15 +3,11 @@ package fr.noahboos.essor.event;
 import fr.noahboos.essor.component.EquipmentLevelingData;
 import fr.noahboos.essor.component.ModDataComponentTypes;
 import fr.noahboos.essor.component.challenge.ChallengesFactory;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.List;
 
 import static fr.noahboos.essor.utils.Constants.UPGRADABLE_ITEM_CLASSES;
 import static fr.noahboos.essor.utils.Constants.UPGRADABLE_ITEM_CLASSES_NO_ARMOUR;
@@ -59,44 +55,4 @@ public class ItemEvents {
         }
     }
 
-    @SubscribeEvent
-    public static void OnItemTooltip(ItemTooltipEvent event) {
-        // Référence du tooltip affiché.
-        List<Component> tooltip = event.getToolTip();
-        // Récupération de l'item que le joueur survole dans son inventaire.
-        ItemStack hoveredItem = event.getItemStack();
-
-        // Si l'item possède le composant de données "DC_EQUIPMENT_LEVELING_DATA", on ajoute au tooltip les informations relatives à l'expérience de l'item.
-        if (hoveredItem.has(ModDataComponentTypes.DC_EQUIPMENT_LEVELING_DATA)) {
-            EquipmentLevelingData hoveredItemData = hoveredItem.get(ModDataComponentTypes.DC_EQUIPMENT_LEVELING_DATA);
-            if (hoveredItemData == null) return;
-
-            StringBuilder prestigeProgressBar = new StringBuilder();
-            int prestigeSegments = 10;
-            int prestigeFilledSegments = hoveredItemData.GetPrestige();
-            prestigeProgressBar.append("§6★".repeat(Math.max(0, prestigeFilledSegments)));
-            prestigeProgressBar.append("§7☆".repeat(Math.max(0, prestigeSegments - prestigeFilledSegments)));
-            tooltip.add(Component.empty());
-            tooltip.add(Component.literal(Component.translatable("tooltip.essor.prestige", hoveredItemData.GetPrestige()).getString() + prestigeProgressBar.toString()));
-            tooltip.add(Component.translatable("tooltip.essor.experienceMultiplier", hoveredItemData.GetExperienceMultiplier()));
-            tooltip.add(Component.empty());
-            tooltip.add(Component.translatable("tooltip.essor.level", hoveredItemData.GetLevel(), hoveredItemData.GetCurrentExperience(), hoveredItemData.GetRequiredExperienceToLevelUp()));
-            StringBuilder levelProgressBar = new StringBuilder();
-            int levelSegments = 25;
-            int levelFilledSegments = (int) (((float) hoveredItemData.GetCurrentExperience() / (float) hoveredItemData.GetRequiredExperienceToLevelUp()) * levelSegments);
-            levelProgressBar.append("§a■".repeat(Math.max(0, levelFilledSegments)));
-            levelProgressBar.append("§7□".repeat(Math.max(0, levelSegments - levelFilledSegments)));
-            tooltip.add(Component.literal(levelProgressBar.toString()));
-            if (UPGRADABLE_ITEM_CLASSES_NO_ARMOUR.contains(hoveredItem.getItem().getClass())) {
-                hoveredItemData.GetChallenges().challenges.forEach(challenge -> {
-                    tooltip.add(Component.empty());
-                    StringBuilder challengeProgressBar = new StringBuilder();
-                    challengeProgressBar.append("§a■".repeat(Math.max(0, challenge.currentTier)));
-                    challengeProgressBar.append("§7□".repeat(Math.max(0, challenge.maximumTier - challenge.currentTier)));
-                    tooltip.add(Component.literal(challenge.id + " - " + challengeProgressBar.toString()));
-                    tooltip.add(Component.literal(challenge.progression + " / " + challenge.tiers.get(challenge.currentTier + 1).toString()));
-                });
-            }
-        }
-    }
 }
